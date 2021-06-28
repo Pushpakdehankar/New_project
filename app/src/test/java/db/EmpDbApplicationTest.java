ca01d0dafcf304462fcf89fac47e9575798b6289
Pushpakdehankar <56184210+Pushpakdehankar@users.noreply.github.com>
2021-06-28 14:38:39 +0530

    first commit

---
+++ app/src/test/java/db/EmpDbApplicationTest.java
@@ -0,0 +1,13 @@
+package db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
